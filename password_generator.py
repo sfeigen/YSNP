3cd6fc0e3777c11724b2b1ab9bd8b3abc657a681
Sam Feigenbaum <seigen@live.com>
2016-09-28 18:18:55 -0400

    added count to multiplier for redundancy check

--- password_generator.py
+++ password_generator.py
@@ -4,35 +4,46 @@ def generate():
     return "talking to password_generator"
 
 # complicated test string to test
-test_str = "! a1"
+test_str = "aaa"
 
 # helper method to check for multipliers
 def multiplier(test_str):
+    alpha_count, number_count, whitespace_count, special_count = 0, 0, 0, 0
     alpha, number, whitespace, special = False, False, False, False
     for i in test_str:
         if i in string.ascii_letters:
             alpha = True
+            alpha_count += 1
         if i in string.digits:
             number = True
+            number_count += 1
         if i in string.whitespace:
             whitespace = True
+            whitespace_count += 1
         if i in string.punctuation:
             special = True
-    return alpha + number + whitespace + special
+            special_count += 1
+
+    multiplier = alpha + number + whitespace + special
+    count = [alpha_count, number_count, whitespace_count, special_count]
+    return multiplier, count
 
 # check value of a given string            
 def pass_val(test_str):
-    return len(test_str) * multiplier(test_str)
+    return len(test_str) * multiplier(test_str)[0]
 
 def modifier(test_str):
     #regex to figure out if multiplier can be upgraded
-    
-if pass_val(test_str) >= 50:
     return test_str
 
-if pass_val(test_str) < 50 and if pass_val > 10:
-    modifier(test_str)
-    return test_str
+def value_router(test_str):
+    if pass_val(test_str) >= 50:
+        return test_str
 
-if pass_val(test_str) < 10:
-    return test_str = "This shall not pass... try a strong password."
+    if pass_val(test_str) < 50 and pass_val(test_str) > 10:
+        modifier(test_str)
+        return test_str
+
+    if pass_val(test_str) < 10:
+        test_str = "This shall not pass... try a stronger password."
+        return test_str
