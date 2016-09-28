50a16cedc3af12650499e50aafdb938b25dd271b
Sam Feigenbaum <seigen@live.com>
2016-09-28 14:15:42 -0700

    wired up tweepy

--- badbalrog.py
+++ badbalrog.py
@@ -1,2 +1,9 @@
 import tweepy
 from config import *
+from password_generator import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+API = tweepy.API(auth)
+
+API.update_status("POST test")
